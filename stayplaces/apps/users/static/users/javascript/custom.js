405988dd21784b2b40c213b92ff548b512e6058a
Mitchell Morrison <mitchell8morrison@gmail.com>
2017-10-25 20:38:33 -0500

    Minor CSS, and beginnign of google maps search

--- stayplaces/apps/users/static/users/javascript/custom.js
+++ stayplaces/apps/users/static/users/javascript/custom.js
@@ -1,3 +1,21 @@
+function initAutocomplete() {
+    // Create the autocomplete object, restricting the search to geographical
+    // location types.
+    autocomplete = new google.maps.places.Autocomplete(
+        /** @type {!HTMLInputElement} */(document.getElementById('search')),
+        {types: ['geocode']});
+
+    // When the user selects an address from the dropdown, populate the address
+    // fields in the form.
+    autocomplete.addListener('place_changed', fillInAddress);
+  }
+
+  function fillInAddress() {
+    // Get the place details from the autocomplete object.
+    var place = autocomplete.getPlace();
+
+  }
+
 $(function() {
     
         $('#login-form-link').click(function(e) {
@@ -16,4 +34,5 @@ $(function() {
         });
     
     });
+
     
